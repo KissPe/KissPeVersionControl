7ce8bb24433c8e1f46cb6804086455caf224e658
Patrik Kiss <patrik.kiss9@gmail.com>
2020-10-11 19:06:11 +0200

    5

--- week5working/week5working/Form1.Designer.cs
+++ week5working/week5working/Form1.Designer.cs
@@ -49,7 +49,6 @@
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(190, 97);
             this.dataGridView2.TabIndex = 1;
-            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // Form1
             // 

0489dd86daae052f1fc0859de5b8b5f2c365685c
Patrik Kiss <patrik.kiss9@gmail.com>
2020-10-18 21:05:49 +0200

    4

---
+++ week6/week6/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week6.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; internal set; }
+        public string Currency { get; internal set; }
+        public decimal Value { get; internal set; }
+    }
+}

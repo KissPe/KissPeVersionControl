79fe0229c30aff7b7de0f73e2bdf6834b490531c
Patrik Kiss <patrik.kiss9@gmail.com>
2020-11-15 18:46:25 +0100

    7

--- week08/week08/Entities/BallFactory.cs
+++ week08/week08/Entities/BallFactory.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using week08.Abstraction;
 
 namespace week08.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
+
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
+        }
+
+        Toy IToyFactory.CreateNew()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ week08/week08/Entities/Carfactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Abstraction;
+
+namespace week08.Entities
+{
+    public class Carfactory
+    {
+        public class CarFactory : IToyFactory
+
+        {
+            public Toy CreateNew()
+            {
+                return new Ball();
+            }
+
+            Toy IToyFactory.CreateNew()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}

4a59bdaa2f97bf9099993f2693b6b9b49723403b
Patrik Kiss <patrik.kiss9@gmail.com>
2020-11-15 18:55:48 +0100

    9

--- week08/week08/Form1.cs
+++ week08/week08/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using week08.Abstraction;
 using week08.Entities;
+using static week08.Entities.Carfactory;
 
 namespace week08
 {
@@ -52,6 +53,47 @@ namespace week08
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+        private Toy _nextToy;
+
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
